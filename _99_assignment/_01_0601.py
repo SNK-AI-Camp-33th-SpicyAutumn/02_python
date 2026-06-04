2672598018cb528a5d3d6e84e061235f74c791e2
SpicyAutumn <ske0683@gmail.com>
2026-06-04 17:49:06 +0900

    20260601_assignment

---
+++ _99_assignment/_01_0601.py
@@ -0,0 +1,70 @@
+print("--- 변수_문제1 ---")
+age = 39
+name = "신가을"
+print("age: ", age, type(age))
+print("name: ", name, type(name))
+
+print("--- 변수_문제2 ---")
+height = 163
+dream = "AI를 활용할 수 있는 행정전문가"
+print("height: ", height, type(height))
+print("dream: ", dream, type(dream))
+
+print("--- 변수_문제3 ---")
+city = "서울특별시"
+my_city = city
+print("city: ", city, type(city))
+print("my_city: ", my_city, type(my_city))
+
+print("--- 변수_문제4 ---")
+apple_count = 10
+orange_count = 20
+print("apple_count: ", apple_count, type(apple_count))
+print("orange_count: ", orange_count, type(orange_count))
+
+print("--- 변수_문제5 ---")
+first_name = "가을"
+last_name = "신"
+full_name = first_name + " " + last_name
+print("full_name: ", full_name, type(full_name))
+
+print("--- 기본 자료형_문제1 ---")
+num1 = 10
+num2 = 5
+sum_result = num1 + num2
+print("sum_result: ", sum_result, type(sum_result))
+
+print("--- 기본 자료형_문제2 ---")
+x = 7.5
+y = 2
+result = x / y
+print("result: ", result, type(result))
+
+print("--- 기본 자료형_문제3 ---")
+is_student = True
+print("is_student: ", is_student, type(is_student))
+
+print("--- 기본 자료형_문제4 ---")
+num1 = 15
+num2 = 4
+print("num1 / num2: ", num1 / num2, type(num1 / num2))
+print("num1 % num2: ", num1 % num2, type(num1 % num2))
+
+print("--- 기본 자료형_문제5 ---")
+greeting_message = "Hello"
+greeting_target = "World"
+greeting_sum = greeting_message + greeting_target
+print("greeting_sum: ", greeting_sum, type(greeting_sum))
+
+print("--- 기본 자료형_문제6 ---")
+base = 2
+exponent = 5
+print("base ** exponent: ", base ** exponent, type(base ** exponent))
+
+print("--- 기본 자료형_문제7 ---")
+number = 3
+print("number % 2: ", number % 2, type(number % 2)) # 나머지가 1이면 홀수
+
+print("--- 기본 자료형_문제8 ---")
+age = 39
+city = "서울특별시"
